36e2c61692ef05022ee51d2d21c3b6ee9e67131f
Cesar Betancourt <cessarr862@gmail.com>
2025-06-02 22:46:11 -0500

    Add - Main menu

--- Untilted Mobile Game/Assets/Scripts/UI/MainMenu.cs
+++ Untilted Mobile Game/Assets/Scripts/UI/MainMenu.cs
@@ -1,23 +1,96 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("MAIN MENU PANELS")]
     [SerializeField] private GameObject optionsPanel;
     [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject exitPanel;
+
+    [SerializeField] private GameObject darkPanel;
+
+    [Header("MAIN MENU BUTTONS")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button exitButton;
+
+    [Header("EXTRA BUTTONS")]
+    [SerializeField] private Button exitGameButton;
+    [SerializeField] private Button[] backButtons;
+
+    [SerializeField] private List<GameObject> panels = new List<GameObject>();
+
     void Start()
     {
-        
+        AssignButtonEvents();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    #region PLAY - EXIT METHODS
     public void LoadGame()
     {
-        SceneManager.LoadScene("Loading Game Screen");
+        SceneManager.LoadScene("Loading Game");
     }
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region BUTTON EVENTS & PANELS ASSIGNMENT
+    private void AssignButtonEvents()
+    {
+        playButton.onClick.AddListener(LoadGame);
+
+        optionsButton.onClick.AddListener(ShowOptions);
+        creditsButton.onClick.AddListener(ShowCredits);
+        exitButton.onClick.AddListener(ShowExit);
+
+        exitGameButton.onClick.AddListener(ExitGame);
+
+        foreach (Button button in backButtons)
+        {
+            button.onClick.AddListener(HidePanels);
+        }
+
+        panels.Add(optionsPanel);
+        panels.Add(creditsPanel);
+        panels.Add(exitPanel);
+    }
+    #endregion BUTTON EVENTS & PANELS ASSIGNMENT
+
+    #region SHOW SELECTION METHODS
+    private void ShowOptions()
+    {
+        darkPanel.SetActive(true);
+        optionsPanel.SetActive(true);
+    }
+
+    private void ShowCredits()
+    {
+        darkPanel.SetActive(true);
+        creditsPanel.SetActive(true);
+    }
+
+    private void ShowExit()
+    {
+        darkPanel.SetActive(true);
+        exitPanel.SetActive(true);
+    }
+    #endregion SHOW SELECTION METHODS
+
+    #region GO BACK METHODS
+    private void HidePanels()
+    {
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+
+        darkPanel.SetActive(false);
+    }
+    #endregion GO BACK METHODS
 }
