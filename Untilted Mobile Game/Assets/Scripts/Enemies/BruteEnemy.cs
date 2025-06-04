236458fdcb2512d54befc26bcf0e97d32b86554c
Cesar Betancourt <cessarr862@gmail.com>
2025-06-03 22:47:53 -0500

    Add - Brute enemy run behaviour

--- Untilted Mobile Game/Assets/Scripts/Enemies/BruteEnemy.cs
+++ Untilted Mobile Game/Assets/Scripts/Enemies/BruteEnemy.cs
@@ -1,57 +1,119 @@
+using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BruteEnemy : EnemyBase
 {
-    [Header("--- BRUTE ENEMY ---\n")]
+    [Header("--- BRUTE SETTINGS ---")]
 
-    [Header("MAIN ABILITY SETTINGS")]
     [SerializeField, Range(2f, 6f)] private int beforeRunTime;
-    [SerializeField] private float runningTime;
+    [SerializeField, Range(2f, 6f)] private int runningTime;
+    [SerializeField, Range(2f, 6f)] private int runCooldownTime;
 
+    [SerializeField] private int minimumDistanceToRun;
     private float playerDistance;
 
-    private bool isRunning;
-    private bool abilityActive;
+    [SerializeField] private bool isRunning;
+    private bool runAbilityActive;
+
+    private Quaternion currentLookAtPlayer;
 
     protected override void Update()
     {
-        if (!isRunning)
-        {
-            base.Update(); 
-        }
+        base.Update();
 
-        Behaviour();
+        RampageRun();
+        RampageRunCheck();
+        GetCurrentLookAtPlayerRot();
     }
 
-    private void Behaviour()
+    private void GetCurrentLookAtPlayerRot()
+    {
+        if (runAbilityActive)
+        {
+            Vector3 lookDirection = player_transform.position - transform.position;
+            Quaternion lookTarget = Quaternion.LookRotation(lookDirection);
+            currentLookAtPlayer = Quaternion.Euler(0, lookTarget.eulerAngles.y, 0); 
+        }
+    }
+
+    protected override void FollowPlayer()
+    {
+        if (!runAbilityActive)
+        {
+            base.FollowPlayer(); 
+        }
+    }
+
+    protected override void LookAtPlayer()
+    {
+        if (!isRunning)
+        {
+            base.LookAtPlayer(); 
+        }
+    }
+
+    private void RampageRunCheck()
     {
         playerDistance = Vector3.Distance(transform.position, player_transform.position);
 
         Vector3 direction = player_transform.position - transform.position;
 
-        if (playerDistance > 4)
+        if ((!runAbilityActive && !isRunning) && playerDistance > minimumDistanceToRun)
         {
-            isRunning = true;
-            agent.destination = agent.transform.position + agent.transform.forward * 2f;
-        }
-        else
-        {
-            isRunning = false;
+            runAbilityActive = true;
+            StartCoroutine(StartRampageRun());
         }
     }
 
-    private IEnumerator RampageRun()
+    private IEnumerator StartRampageRun()
     {
+        Debug.Log("Entered");
+        //Stops following the player for a period of time
+        agent.destination = transform.position; 
         yield return new WaitForSeconds(beforeRunTime);
+
+        //While looking at the players position, it moves towards it at a fast pace during a period of time.
+        isRunning = true;
+        agent.speed = 15f;
+        yield return new WaitForSeconds(runningTime);
+
+        //Sets speed to 0 and waits until the enemy slows down.
+        agent.speed = 0f;
+        yield return new WaitUntil(() => agent.velocity.magnitude == 0);
+
+        //Looks smoothly at player before returning to its default state
+        float time = 0f;
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, currentLookAtPlayer, time);
+            time += Time.deltaTime * 0.6f;
+            yield return null;
+        }
+        transform.rotation = currentLookAtPlayer;
+
+        //Stops running and looks at player again but remains steady for a period of time.
+        isRunning = false;
+        agent.destination = transform.position;
+        yield return new WaitForSeconds(runCooldownTime);
+
+        runAbilityActive = false;
+    }
+
+    private void RampageRun()
+    {
+        if (isRunning)
+        {
+            agent.destination = agent.transform.position + agent.transform.forward * 5f; 
+        }
     }
 
     private void OnDrawGizmos()
     {
         if (player_transform != null)
         {
-            if (playerDistance > 4)
+            if (playerDistance > minimumDistanceToRun)
             {
                 Gizmos.color = Color.green;
             }
@@ -59,9 +121,7 @@ public class BruteEnemy : EnemyBase
             {
                 Gizmos.color = Color.red;
             }
-
             Gizmos.DrawLine(transform.position, player_transform.position); 
         }
-
     }
 }
